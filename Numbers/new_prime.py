59d2714f448bebd149f5886abee30d503fbcb3bc
Chris Lucas <chris.lucas@bristol.ac.uk>
2014-09-24 17:40:59 +0100

    move functions to utils file; add some brief comments

--- Numbers/new_prime.py
+++ Numbers/new_prime.py
@@ -1,43 +1,15 @@
 import math as ma
-
-def is_prime(val = None):
-
-    if val == 1:
-        return False
-    for i in range(2, 1+val/2):
-        if val%i == 0:
-            return False
-
-    return True
-
-def choice(text = "Do?", default = ""):
-
-    if default == "y":
-        choices = "Y/n"
-    elif default == "n":
-        choices = "y/N"
-    else:
-        choices = "y/n"
-
-    while True:
-        r_in = raw_input("%s [%s]" % (text, choices))
-
-        if not r_in:
-            r_in = default
-
-        if r_in in ['y', 'Y', 'yes', 'Yes']:
-            return True
-        elif r_in in ['n', 'N', 'no', 'No']:
-            return False
-
+import utils
 
 if __name__ == "__main__":
 
+    # start the number at 1
     my_num = 1
     
     while True:
-        if is_prime(my_num):
+        if utils.is_prime(my_num):
             print "> Prime number found: %d" % my_num
-            if not choice("Find another?", "y"):
+            if not utils.choice("Find another?", "y"):
                 break
+        # increment is prime not found or user wants to continue
         my_num += 1
