a7476d5022584ea9fb0406b4ca6fb85f8ce111fa
Chris Lucas <chris.lucas@bristol.ac.uk>
2014-09-24 17:41:19 +0100

    start populating a utils file

---
+++ Numbers/utils.py
@@ -0,0 +1,31 @@
+import math as ma
+
+def is_prime(val = None):
+
+    if val == 1:
+        return False
+    for i in range(2, 1+val/2):
+        if val%i == 0:
+            return False
+
+    return True
+
+def choice(text = "Do?", default = ""):
+
+    if default == "y":
+        choices = "Y/n"
+    elif default == "n":
+        choices = "y/N"
+    else:
+        choices = "y/n"
+
+    while True:
+        r_in = raw_input("%s [%s]" % (text, choices))
+
+        if not r_in:
+            r_in = default
+
+        if r_in in ['y', 'Y', 'yes', 'Yes']:
+            return True
+        elif r_in in ['n', 'N', 'no', 'No']:
+            return False
